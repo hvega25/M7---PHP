d4e30bed29223d8a932c04df16a279c3b626fa80
hvega25 <hvega@jaumebalmes.net>
2022-10-04 20:15:45 +0200

    a

---
+++ Activitat1/activitat1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $nom = "John";
+    $cognoms = "Doe";
+    $edat = 30;
+    $data = "1986-03-11";
+    $telefon = "93555555";
+    $adreca = "Blackpool, England";
+    $email = "oswin@dr.who";
+    $treballa = False;
+    $alcada = 170;
+
+
+ 
+    echo "Descrivint a $nom $cognoms";
+?>
+
+</body>
+</html>
---
+++ practica_2/practica.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array (
+  array(rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5)),
+  array(rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5)),
+  array(rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5)),
+  array(rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5)),
+  array(rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5), rand(0,5))
+);
+  
+echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+?>
+
+</body>
+</html>
